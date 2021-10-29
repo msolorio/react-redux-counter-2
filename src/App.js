e383e6e9c6e710befb4663d7c9fab3091d31386d
msolorio <msolorio.github@gmail.com>
2021-10-29 16:22:12 -0700

    Adds code react redux counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from './features/counter/counterSlice';
 import './App.css';
 
 function App() {
+  const count = useSelector(state => state.counter.value);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+
+      <p>Count: {count}</p>
+
+      <button onClick={() => dispatch(increment())}>
+        Increment
+      </button>
     </div>
   );
 }
